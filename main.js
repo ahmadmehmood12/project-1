567f7272cee7f675796d1f193531845812f9160c
ahmadmehmood12 <104225591+ahmadmehmood12@users.noreply.github.com>
2022-08-08 12:01:01 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+   $("#testimonals").click(function(){
+    $(".a1").html('<p class="text-center px-5 text-white fw-bold a1">cing elit. Quidem, veritatis nulla eum laudantium totam tempore optio doloremque laboriosam quas, quos eaque molestias odio aut eius animi. Impedit temporibus nisi accusamus.</p>');
+  });
+});
